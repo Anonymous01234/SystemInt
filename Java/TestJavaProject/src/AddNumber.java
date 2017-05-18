eacac215a0dc00e45e5fadbdc1f4944ee1f93c1a
Anonymous01234 <Anonymous01234@users.noreply.github.com>
2017-05-18 11:43:47 +0530

    Adding new java project.

---
+++ Java/TestJavaProject/src/AddNumber.java
@@ -0,0 +1,22 @@
+
+public class AddNumber {
+
+	public AddNumber() {
+		// TODO Auto-generated constructor stub
+		int a,b,result;
+		a = 5;
+		b=10;
+		result = a + b;
+		System.out.println(result);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddNumber aInstance = new AddNumber();
+
+	}
+
+}
